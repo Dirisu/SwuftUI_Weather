2c53c1b59b855752038f67fc14440c898a990094
Dirisu <dirisumarvellous94@gmail.com>
2023-09-12 22:08:30 +0100

    Initial Commit

---
+++ SwuftUI_Weather/SwuftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwuftUI_WeatherApp.swift
+//  SwuftUI_Weather
+//
+//  Created by Marvellous Dirisu on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwuftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
